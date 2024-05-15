22389df5e4dc0acf435ac2a10ca5b5f814ffe053
Garrett-Floyd <Garrett.Floyd.Github@gmail.com>
2024-05-14 23:31:30 -0400

    Add functionality to play multiround games
    Forgot to mention last time that althought the assignment calls for
    player and npc scores to be global variables, I didn't do that because
    it unneccesarily breaks encapsilation. Functionality for the user to
    choose the number of rounds played was also added.

--- js/script.js
+++ js/script.js
@@ -54,35 +54,63 @@ function playRound(humanChoice,computerChoice){
     let roundOutcome = "";
 
     if (Math.round(humanChoiceInt % 3) === Math.round(computerChoiceInt % 3)) {
-        console.log("It's a draw! You both choose " + humanChoice);
+        console.log("This round is a draw! You both choose " + humanChoice);
         roundOutcome = "draw";        
     } else if (Math.round((humanChoiceInt + 1) % 3) === Math.round(computerChoiceInt % 3)) {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);        
+        console.log("You lose this round! " + computerChoice + " beats " + humanChoice);        
         roundOutcome = "loss";
     } else if (Math.round((humanChoiceInt + 2) % 3) === Math.round(computerChoiceInt % 3)) {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        console.log("You win this round! " + humanChoice + " beats " + computerChoice);
         roundOutcome = "win";        
     } else {
-        console.log("Warning the PlayRound function has detected a bug.");
+        console.log("WARNING: Error detected in PlayRound function!");
     }
     
     return roundOutcome;
 }
 
-function main(){
+function playGame(num_rounds){
     let computerScore = 0;
     let humanScore = 0;
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    const roundOutcome = playRound(humanSelection, computerSelection);
-    if  (roundOutcome === "loss"){
-        ++computerScore;
-    } else if (roundOutcome == "win"){
-        ++humanScore;
+    for (let round = 0; round < num_rounds; ++round){
+        if (round > 0) {
+            console.log("The current score is " + humanScore + " vs " + computerScore);   
+        }
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const roundOutcome = playRound(humanSelection, computerSelection);
+        if  (roundOutcome === "loss"){
+            ++computerScore;
+        } else if (roundOutcome == "win"){
+            ++humanScore;
+        }        
     }
 
-    console.log("The score is " + humanScore + " vs. " + computerScore);
+    console.log("The final score is " + humanScore + " vs " + computerScore); 
+    if (humanScore > computerScore) {
+        console.log("Congrats! You won the game");
+    } else if (humanScore < computerScore){
+        console.log("You lost the game.");
+    } else if (humanScore === computerScore){
+        console.log("The game is a draw.");
+    } else {
+        console.log("WARNING: Error dectected in playGame function!");
+    }
+}
+
+function main(){
+    keepGoing = true;
+    while (keepGoing) {
+        let num_rounds = prompt("how many rounds of rock paper scissors would you like to play?");
+        if (Number.isInteger(Number(num_rounds))) {
+            playGame(num_rounds);
+            keepGoing = false;          
+        }
+        else{
+            console.log("That is not a valid number of rounds. Please enter an integer.");
+        }
+    }
 }
 
 main();
